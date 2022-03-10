5036331012a00d1064039089b5160c4056dae50c
elgamalsalman <elgamalsalman@gmail.com>
2022-03-10 15:54:03 +0200

    added asset images

--- public/js/charter.js
+++ public/js/charter.js
@@ -1,21 +1,19 @@
-const chart1 = LightweightCharts.createChart(document.querySelector(".chart1"), {width: 400, height: 300});
-const chart2 = LightweightCharts.createChart(document.querySelector(".chart2"), {width: 400, height: 300});
-const candlestick_series1 = chart1.addCandlestickSeries();
-const candlestick_series2 = chart2.addCandlestickSeries();
+const price_chart = LightweightCharts.createChart(document.querySelector(".prices-chart"), {width: 400, height: 300});
+const candlestick_series = price_chart.addCandlestickSeries();
 
 const ws = new WebSocket(`ws://${window.location.hostname}:8080/ws/btcusdt`);
 
 ws.onmessage = data => {
 	data = JSON.parse(data.data);
 	if (data.exchange === "BINANCE") {
-		console.log(data);
+		// console.log(data);
 		for (const candle of data.candles) {
-			candlestick_series1.update(candle);
+			candlestick_series.update(candle);
 		}
 	} else if (data.exchange === "KRAKEN") {
-		console.log(data);
-		for (const candle of data.candles) {
-			candlestick_series2.update(candle);
-		}
+		// console.log(data);
+		// for (const candle of data.candles) {
+		// 	candlestick_series.update(candle);
+		// }
 	}
 };
